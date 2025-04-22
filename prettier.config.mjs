c70f8ce189778f819bfbc6494ac020fd98bd3e7c
Rishiraj Dhar <rishirajdhr@gmail.com>
2025-04-22 14:21:06 -0400

    build: add prettier to the project

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+    arrowParens: "always",
+    bracketSameLine: false,
+    bracketSpacing: true,
+    objectWrap: "collapse",
+    printWidth: 80,
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4
+}
+
+export default config;
