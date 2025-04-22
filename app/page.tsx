af4c643c93a22c5cfd95bfb6d935316825607777
Rishiraj Dhar <rishirajdhr@gmail.com>
2025-04-22 15:56:38 -0400

    feat: add feature flag for deployment environment

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="flex justify-center items-center h-screen w-screen">
+        <div className="flex h-screen w-screen items-center justify-center">
             <main>
-                <Image src="/under-construction.jpg" alt="This website is under construction" width={600} height={600} />
+                {process.env.APP_ENV == "production" ? (
+                    <Image
+                        src="/under-construction.jpg"
+                        alt="This website is under construction"
+                        width={600}
+                        height={600}
+                    />
+                ) : (
+                    <h1 className="text-2xl">Rishiraj's Portfolio</h1>
+                )}
             </main>
         </div>
     );
